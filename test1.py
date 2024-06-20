975bf9589f4df8e29d183fb8f52b7daaed2c9394
jiapengyu113599 <160700180+jiapengyu113599@users.noreply.github.com>
2024-06-20 11:15:23 +0800

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,45 +1,41 @@
-import requests
+import streamlit as st
 from bs4 import BeautifulSoup
+import requests
 import re
-import json
-import os
-
-def getHtml(baseurl):
-    head = {    #模拟浏览器身份头向对方发送消息
-        "user-agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.56 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.56"
-    }
-    try:
-        response = requests.get(url = baseurl, headers = head)
-        if response.status_code==200:
-            return response.content.decode("utf-8")
-    except:
-        print("请求失败")
+import jieba
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import numpy as np
 
 def main():
-    print("这是bv号弹幕获取软件")
-    bv = input("请输入bv号: ")
-    p = int(input("请输入p号: "))
-    baseurl = "https://www.bilibili.com/video/BV"+str(bv)+"?p="+str(p)
-    # 获取网页的源代码
-    req=getHtml(baseurl)
-    # cid所在的位置是window.__INITIAL_STATE__={}里面，所以要在这里找各个cid号
-    pattern = r'\<script\>window\.__INITIAL_STATE__=(.*?)\</script\>'
-    # result结果得是string
-    result = re.findall(pattern, req)[0]
-    match_rule = r'{"cid":(.*?),'
-    cid = re.findall(match_rule, result)
-    # 打开弹幕资源位置
-    baseurl = "https://api.bilibili.com/x/v1/dm/list.so?oid="+str(cid[p-1])
-    req = getHtml(baseurl)
-    soup = BeautifulSoup(req,'html.parser')
-    match_rule = r'<d p=".*">(.*?)</d>'
-    cid = ""
-    for item in soup.find_all('d'):
-        item = str(item)
-        danmu = re.findall(match_rule, item)[0]
-        cid = cid + danmu + " "
-    print(cid)
-    os.system("pause")
+    st.title('Bilibili DanMu Word Cloud')
+
+    # 爬取网页源码
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+    }
+    response = requests.get("https://api.bilibili.com/x/v1/dm/list.so?oid=130684654", headers=headers)
+    html_doc = response.content.decode('utf-8')
+
+    # 弹幕匹配
+    format = re.compile(r'<d.*?>(.*?)</d>')
+    DanMu_list = format.findall(html_doc)
+
+    # 合并弹幕
+    DanMu_text = ' '.join(DanMu_list)
+
+    # 分词
+    words = jieba.lcut(DanMu_text)
+
+    # 生成词云图
+    wordcloud = WordCloud(font_path='simhei.ttf', background_color='white', max_words=100, max_font_size=100).generate(' '.join(words))
+
+    # 显示词云图
+    fig = plt.figure(figsize=(10, 8), facecolor=None)
+    plt.imshow(wordcloud)
+    plt.axis("off")
+    plt.tight_layout(pad=0)
+    st.pyplot(fig)
 
 if __name__ == "__main__":
     main()
